02730e4346ba8da58497785ecab593c709c97706
madsing98 <7188647+madsing98@users.noreply.github.com>
2020-05-14 18:24:37 +0800

    Version using TFT_eSPI and jpeg images

--- YukariInfoDisp.ino
+++ YukariInfoDisp.ino
@@ -1,332 +1,96 @@
-/**************************************************************************
-  This is a library for several Adafruit displays based on ST77* drivers.
-  Works with the Adafruit 1.8" TFT Breakout w/SD card
-    ----> http://www.adafruit.com/products/358
-  The 1.8" TFT shield
-    ----> https://www.adafruit.com/product/802
-  The 1.44" TFT breakout
-    ----> https://www.adafruit.com/product/2088
-  The 1.14" TFT breakout
-  ----> https://www.adafruit.com/product/4383
-  The 1.3" TFT breakout
-  ----> https://www.adafruit.com/product/4313
-  The 1.54" TFT breakout
-    ----> https://www.adafruit.com/product/3787
-  The 2.0" TFT breakout
-    ----> https://www.adafruit.com/product/4311
-  as well as Adafruit raw 1.8" TFT display
-    ----> http://www.adafruit.com/products/618
-  Check out the links above for our tutorials and wiring diagrams.
-  These displays use SPI to communicate, 4 or 5 pins are required to
-  interface (RST is optional).
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  MIT license, all text above must be included in any redistribution
- **************************************************************************/
+/*====================================================================================
 
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+  This sketch demonstrates loading images which have been stored as files in the
+  built-in FLASH memory on a NodeMCU 1.0 (ESP8266 based, ESP-12E Module) rendering the
+  images onto a 160 x 128 pixel TFT screen.
+
+  The images are stored in the SPI FLASH Filing System (SPIFFS), which effectively
+  functions like a tiny "hard drive". This filing system is built into the ESP8266
+  Core that can be loaded from the IDE "Boards manager" menu option. This is at
+  version 2.3.0 at the time of sketch creation.
+
+  The size of the SPIFFS partition can be set in the IDE as 1Mbyte or 3Mbytes. Either
+  will work with this sketch. Typically most sketches easily fit within 1 Mbyte so a
+  3 Mbyte SPIFS partition can be used, in which case it can contain 100's of Jpeg
+  full screem images.
+
+  The Jpeg library can be found here:
+  https://github.com/Bodmer/JPEGDecoder
+ 
+  Images in the Jpeg format can be created using Paint or IrfanView or other picture
+  editting software.
+
+  Place the images inside the sketch folder, in a folder called "Data".  Then upload
+  all the files in the folder using the Arduino IDE "ESP8266 Sketch Data Upload" option
+  in the "Tools" menu:
+  http://www.esp8266.com/viewtopic.php?f=32&t=10081
+  https://github.com/esp8266/arduino-esp8266fs-plugin/releases
+  
+  This takes some time, but the SPIFFS content is not altered when a new sketch is
+  uploaded, so there is no need to upload the same files again!
+  Note: If open, you must close the "Serial Monitor" window to upload data to SPIFFS!
+
+  The IDE will not copy the "data" folder with the sketch if you save the sketch under
+  another name. It is necessary to manually make a copy and place it in the sketch
+  folder.
+
+  This sketch includes example images in the Data folder.
+
+  Saving images, uploading and rendering on the TFT screen couldn't be much easier!
+
+  Created by Bodmer 24th Jan 2017 - Tested in Arduino IDE 1.8.0 esp8266 Core 2.3.0
+  ==================================================================================*/
+
+//====================================================================================
+//                                  Libraries
+//====================================================================================
+// Call up the SPIFFS FLASH filing system this is part of the ESP Core
+#include <SPIFFS.h>
+
+// JPEG decoder library
+#include <JPEGDecoder.h>
+
+// SPI library, built into IDE
 #include <SPI.h>
-#include "SPIFFS.h"
 
-#define TFT_CS 5
-#define TFT_RST 4
-#define TFT_DC 2
+// Call up the TFT library
+#include <TFT_eSPI.h> // Hardware-specific library for ESP8266
+// The TFT control pins are set in the User_Setup.h file <<<<<<<<<<<<<<<<< NOTE!
+// that can be found in the "src" folder of the library
 
-// OPTION 1 (recommended) is to use the HARDWARE SPI pins, which are unique
-// to each board and not reassignable. For Arduino Uno: MOSI = pin 11 and
-// SCLK = pin 13. This is the fastest mode of operation and is required if
-// using the breakout board's microSD card.
+// Invoke TFT library
+TFT_eSPI tft = TFT_eSPI();
 
-// For 1.44" and 1.8" TFT with ST7735 use:
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
-
-// For 1.14", 1.3", 1.54", and 2.0" TFT with ST7789:
-//Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
-
-// OPTION 2 lets you interface the display using ANY TWO or THREE PINS,
-// tradeoff being that performance is not as fast as hardware SPI above.
-//#define TFT_MOSI 11  // Data out
-//#define TFT_SCLK 13  // Clock out
-
-// For ST7735-based displays, we will use this call
-//Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
-
-// OR for the ST7789-based displays, we will use this call
-//Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
-
-float p = 3.1415926;
-
-void setup(void)
+//====================================================================================
+//                                    Setup
+//====================================================================================
+void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(115200); // Used for messages and the C array generator
 
-  if (!SPIFFS.begin(true))
-  {
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
+  delay(10);
+  Serial.println("NodeMCU decoder test!");
+
+  tft.begin();
+  tft.setRotation(0);  // 0 & 2 Portrait. 1 & 3 landscape
+  tft.fillScreen(TFT_BLACK);
+
+  if (!SPIFFS.begin()) {
+    Serial.println("SPIFFS initialisation failed!");
+    while (1) yield(); // Stay here twiddling thumbs waiting
   }
+  Serial.println("\r\nInitialisation done.");
 
-  Serial.print("Total Bytes: ");
-  Serial.println(SPIFFS.totalBytes());
-  Serial.print("Used Bytes: ");
-  Serial.println(SPIFFS.usedBytes());
+  // Note the / before the SPIFFS file name must be present, this means the file is in
+  // the root directory of the SPIFFS, e.g. "/Tiger.jpg" for a file called "Tiger.jpg"
 
-  File root = SPIFFS.open("/"); // open file system main root
-  printDirectory(root, 0);      // print all files names and sizes
-  root.close();                 // close the opened root
-
-  Serial.print(F("Hello! ST77xx TFT Test"));
-
-  // Use this initializer if using a 1.8" TFT screen:
-  //tft.initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
-
-  // OR use this initializer if using a 1.8" TFT screen with offset such as WaveShare:
-  // tft.initR(INITR_GREENTAB);      // Init ST7735S chip, green tab
-
-  // OR use this initializer (uncomment) if using a 1.44" TFT:
-  //tft.initR(INITR_144GREENTAB); // Init ST7735R chip, green tab
-
-  // OR use this initializer (uncomment) if using a 0.96" 160x80 TFT:
-  tft.initR(INITR_MINI160x80); // Init ST7735S mini display
-
-  // OR use this initializer (uncomment) if using a 1.3" or 1.54" 240x240 TFT:
-  //tft.init(240, 240);           // Init ST7789 240x240
-
-  // OR use this initializer (uncomment) if using a 2.0" 320x240 TFT:
-  //tft.init(240, 320);           // Init ST7789 320x240
-
-  // OR use this initializer (uncomment) if using a 1.14" 240x135 TFT:
-  //tft.init(135, 240);           // Init ST7789 240x135
-
-  // SPI speed defaults to SPI_DEFAULT_FREQ defined in the library, you can override it here
-  // Note that speed allowable depends on chip and quality of wiring, if you go too fast, you
-  // may end up with a black screen some times, or all the time.
-  //tft.setSPISpeed(20000000);
-
-  Serial.println(F("Initialized"));
-
-  uint32_t time = millis();
-  tft.fillScreen(ST77XX_BLACK);
-  time = millis() - time;
-  Serial.println(time, DEC);
-
-  char bmpName[20];
-  uint8_t i;
-
-  while (true)
-  {
-    for (i=1;i<150;i++)
-    {
-      time = millis();
-      sprintf(bmpName, "/tokyo%03d.bmp", i);
-      bmpDraw(bmpName, 0, 0);
-      while (millis() < time + 200)
-      {
-      }
-    }
-  }
-
-  Serial.println("done");
+  drawJpeg("/train.jpg", 0, 0);
 }
 
+//====================================================================================
+//                                    Loop
+//====================================================================================
 void loop()
 {
 }
-
-// This function opens a Windows Bitmap (BMP) file and
-// displays it at the given coordinates.  It's sped up
-// by reading many pixels worth of data at a time
-// (rather than pixel by pixel).  Increasing the buffer
-// size takes more of the Arduino's precious RAM but
-// makes loading a little faster.  20 pixels seems a
-// good balance.
-
-#define BUFFPIXEL 40
-
-void bmpDraw(char *filename, uint8_t x, uint16_t y)
-{
-  File bmpFile;
-  int bmpWidth, bmpHeight;            // W+H in pixels
-  uint8_t bmpDepth;                   // Bit depth (currently must be 24)
-  uint32_t bmpImageoffset;            // Start of image data in file
-  uint32_t rowSize;                   // Not always = bmpWidth; may have padding
-  uint8_t sdbuffer[3 * BUFFPIXEL];    // pixel buffer (R+G+B per pixel)
-  uint8_t buffidx = sizeof(sdbuffer); // Current position in sdbuffer
-  boolean goodBmp = false;            // Set to true on valid header parse
-  boolean flip = true;                // BMP is stored bottom-to-top
-  int w, h, row, col;
-  uint8_t r, g, b;
-  uint32_t pos = 0, startTime = millis();
-
-  if ((x >= tft.width()) || (y >= tft.height()))
-    return;
-
-  // Serial.println();
-  // Serial.print(F("Loading image '"));
-  // Serial.print(filename);
-  // Serial.println('\'');
-
-  // Open requested file
-  if ((bmpFile = SPIFFS.open(filename)) == NULL)
-  {
-    Serial.println(F("File not found"));
-    return;
-  }
-
-  // Parse BMP header
-  if (read16(bmpFile) == 0x4D42)
-  { // BMP signature
-    // Serial.print(F("File size: "));
-    // Serial.println(read32(bmpFile));
-    read32(bmpFile);
-    (void)read32(bmpFile);            // Read & ignore creator bytes
-    bmpImageoffset = read32(bmpFile); // Start of image data
-    // Serial.print(F("Image Offset: "));
-    // Serial.println(bmpImageoffset, DEC);
-    // Read DIB header
-    // Serial.print(F("Header size: "));
-    // Serial.println(read32(bmpFile));
-    read32(bmpFile);
-    bmpWidth = read32(bmpFile);
-    bmpHeight = read32(bmpFile);
-    if (read16(bmpFile) == 1)
-    {                             // # planes -- must be '1'
-      bmpDepth = read16(bmpFile); // bits per pixel
-      // Serial.print(F("Bit Depth: "));
-      // Serial.println(bmpDepth);
-      if ((bmpDepth == 24) && (read32(bmpFile) == 0))
-      { // 0 = uncompressed
-
-        goodBmp = true; // Supported BMP format -- proceed!
-        // Serial.print(F("Image size: "));
-        // Serial.print(bmpWidth);
-        // Serial.print('x');
-        // Serial.println(bmpHeight);
-
-        // BMP rows are padded (if needed) to 4-byte boundary
-        rowSize = (bmpWidth * 3 + 3) & ~3;
-
-        // If bmpHeight is negative, image is in top-down order.
-        // This is not canon but has been observed in the wild.
-        if (bmpHeight < 0)
-        {
-          bmpHeight = -bmpHeight;
-          flip = false;
-        }
-
-        // Crop area to be loaded
-        w = bmpWidth;
-        h = bmpHeight;
-        if ((x + w - 1) >= tft.width())
-          w = tft.width() - x;
-        if ((y + h - 1) >= tft.height())
-          h = tft.height() - y;
-
-        // Set TFT address window to clipped image bounds
-        tft.startWrite();
-        tft.setAddrWindow(x, y, w, h);
-
-        for (row = 0; row < h; row++)
-        { // For each scanline...
-
-          // Seek to start of scan line.  It might seem labor-
-          // intensive to be doing this on every line, but this
-          // method covers a lot of gritty details like cropping
-          // and scanline padding.  Also, the seek only takes
-          // place if the file position actually needs to change
-          // (avoids a lot of cluster math in SD library).
-          if (flip) // Bitmap is stored bottom-to-top order (normal BMP)
-            pos = bmpImageoffset + (bmpHeight - 1 - row) * rowSize;
-          else // Bitmap is stored top-to-bottom
-            pos = bmpImageoffset + row * rowSize;
-          if (bmpFile.position() != pos)
-          { // Need seek?
-            tft.endWrite();
-            bmpFile.seek(pos);
-            buffidx = sizeof(sdbuffer); // Force buffer reload
-          }
-
-          for (col = 0; col < w; col++)
-          { // For each pixel...
-            // Time to read more pixel data?
-            if (buffidx >= sizeof(sdbuffer))
-            { // Indeed
-              bmpFile.read(sdbuffer, sizeof(sdbuffer));
-              buffidx = 0; // Set index to beginning
-              tft.startWrite();
-            }
-
-            // Convert pixel from BMP to TFT format, push to display
-            b = sdbuffer[buffidx++];
-            g = sdbuffer[buffidx++];
-            r = sdbuffer[buffidx++];
-            tft.pushColor(tft.color565(r, g, b));
-          } // end pixel
-        }   // end scanline
-        tft.endWrite();
-        // Serial.print(F("Loaded in "));
-        // Serial.print(millis() - startTime);
-        // Serial.println(" ms");
-      } // end goodBmp
-    }
-  }
-
-  bmpFile.close();
-  if (!goodBmp)
-    Serial.println(F("BMP format not recognized."));
-}
-
-// These read 16- and 32-bit types from the file.
-// BMP data is stored little-endian, Arduino is little-endian too.
-// May need to reverse subscript order if porting elsewhere.
-
-uint16_t read16(File f)
-{
-  uint16_t result;
-  ((uint8_t *)&result)[0] = f.read(); // LSB
-  ((uint8_t *)&result)[1] = f.read(); // MSB
-  return result;
-}
-
-uint32_t read32(File f)
-{
-  uint32_t result;
-  ((uint8_t *)&result)[0] = f.read(); // LSB
-  ((uint8_t *)&result)[1] = f.read();
-  ((uint8_t *)&result)[2] = f.read();
-  ((uint8_t *)&result)[3] = f.read(); // MSB
-  return result;
-}
-
-void printDirectory(File dir, int numTabs)
-{
-  while (true)
-  {
-    File entry = dir.openNextFile();
-    if (!entry)
-    {
-      // no more files
-      break;
-    }
-    for (uint8_t i = 0; i < numTabs; i++)
-    {
-      Serial.print('\t');
-    }
-    Serial.print(entry.name());
-    if (entry.isDirectory())
-    {
-      Serial.println("/");
-      printDirectory(entry, numTabs + 1);
-    }
-    else
-    {
-      // files have sizes, directories do not
-      Serial.print("\t\t");
-      Serial.println(entry.size(), DEC);
-    }
-    entry.close();
-  }
-}
+//====================================================================================
